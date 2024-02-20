d96d9fb3ff6da57851a85a2920e43ea9049fffee
Rishbh Arora <vsryarora6879@gmail.com>
2024-02-20 20:36:09 +0530

    connect to MongoDB Atlas

--- myApp.js
+++ myApp.js
@@ -1,5 +1,11 @@
+const mongoose = require('mongoose');
 require('dotenv').config();
-
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connected to MongoDB");
+});
 
 let Person;
 
